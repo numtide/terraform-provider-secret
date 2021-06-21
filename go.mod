5ccff0cc95cb46900f52596f2dd3734ecd1fb5ef
zimbatm <zimbatm@zimbatm.com>
2021-06-21 18:40:05 +0200

    use the terraform plugin SDK
    Switch away from the old Terraform 0.12 protocol

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/tweag/terraform-provider-secret
 
-go 1.12
+go 1.15
 
-require github.com/hashicorp/terraform v0.12.0
+require github.com/hashicorp/terraform-plugin-sdk v1.17.2
--- secret/resource.go
+++ secret/resource.go
@@ -3,7 +3,7 @@ package secret
 import (
 	"fmt"
 
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func resource() *schema.Resource {
